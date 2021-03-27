cec9202f822eb365ba0b54190e4309ca564e4233
Jon <jonathan@newmexicomathematical.org>
2021-03-27 23:22:39 +0000

    fixed error with UserService POM and added getPostById

--- PostService/src/main/java/com/example/PostService/controller/PostController.java
+++ PostService/src/main/java/com/example/PostService/controller/PostController.java
@@ -1,8 +1,23 @@
 package com.example.PostService.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.PostService.model.Post;
+import com.example.PostService.service.PostService;
 
 @RestController
 public class PostController {
-
+	@Autowired
+	private PostService postService;
+	
+	@GetMapping("/posts/{id}")
+	public Post getPostByID(@PathVariable int id) {
+		Post possiblePost = postService.getPostById(id);
+		return possiblePost;
+		
+	}
+	
 }
--- PostService/src/main/java/com/example/PostService/service/PostService.java
+++ PostService/src/main/java/com/example/PostService/service/PostService.java
@@ -1,8 +1,23 @@
 package com.example.PostService.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.example.PostService.model.Post;
+import com.example.PostService.repository.PostRepository;
 
 @Component
 public class PostService {
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post getPostById(int id) {
+		Post post = new Post();
+		post = postRepository.getPostById(id);
+		if( post != null)
+			return post;
+		else
+			return null;
+	}
 
 }
